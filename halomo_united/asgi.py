1941fb41e24fbf45a756961eeef5bce52963920d
elcipeng <tamajustin322@gmail.com>
2025-09-04 21:09:22 +0700

    test

---
+++ halomo_united/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for halomo_united project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'halomo_united.settings')
+
+application = get_asgi_application()
